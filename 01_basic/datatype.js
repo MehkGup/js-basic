56d2bb6063138b9710cada37f1e6e164a81a931c
MehkGup <166231141+MehkGup@users.noreply.github.com>
2024-05-03 19:36:29 +0000

    configure con done

---
+++ 01_basic/datatype.js
@@ -0,0 +1,10 @@
+"use strict"//treat  all js code as newer version
+
+//alert(3+3)//alert donot work in nodejs like this it only used in browser.
+
+let name='mg'
+let age=10;
+let address='gandhinagar'
+let phone;
+let name_school=true;
+console.table(typeof[name,age,address,phone,name_school])
