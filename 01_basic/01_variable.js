56daae32bb2583b5451b9c298a1d55ac3e7d1cf5
MehkGup <166231141+MehkGup@users.noreply.github.com>
2024-05-03 19:22:05 +0000

    configure con done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,14 @@
+//variable in js are of three types
+/* 1. const---> const means constant (fixed value)
+2. let---->value can be changed but variable can not be redeclear
+3. var------> var is also used in variable declearation in older times but now it donot used .
+becuase it change value as well as redeclear*/
+
+const account_id=14559
+let accountEmail='mg@123gmail.com';
+var pass='1569'
+accountcity='delhi';
+// account_id = 9
+console.log(account_id);
+// for not typing multiple times console.log we can use 
+console.table([account_id,accountEmail,pass,accountcity])
